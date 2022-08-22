a0702c384ec7f676ad4e35ba0164a9109a0e5909
Saptarshi Biswas <109581902+saps999@users.noreply.github.com>
2022-08-22 22:36:19 +0530

    Update Leap_year.java

--- src/Leap_year.java
+++ src/Leap_year.java
@@ -5,12 +5,10 @@ public class Leap_year {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the year: ");
         int year = sc.nextInt();
-        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0){
             System.out.println(year + " is a leap year");
-        else
-
-    {
+        } else {
         System.out.println(year + " is not a leap year");
-    }
-}
+        }
+  }
 }
